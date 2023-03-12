de95060cd71eb4d4677310b8ea4c9fb2fd6465aa
Everton Ulisses <everton.ulisses@cassi.com.br>
2023-03-12 00:22:04 -0300

    Criando pacote Repository

---
+++ backend/src/main/java/com/tecnologia/dsmeta/controller/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.tecnologia.dsmeta.controller.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tecnologia.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
