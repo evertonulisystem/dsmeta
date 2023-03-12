bfef20f1898181c85c490dad1c592031eb01824e
Everton Ulisses <everton.ulisses@cassi.com.br>
2023-03-12 00:12:32 -0300

    Definindo o mapeamento JPA

--- backend/src/main/java/com/tecnologia/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/tecnologia/dsmeta/entities/Sale.java
@@ -1,14 +1,18 @@
 package com.tecnologia.dsmeta.entities;
 
 import java.time.LocalDate;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Table
+@Entity
+@Table(name="tb_sales")
 public class Sale {
 	
 	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
 	
 	
